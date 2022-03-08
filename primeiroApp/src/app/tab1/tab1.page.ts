0438380b1e319a5b44557dc5c0678fbc24067b42
Éden Silva <91293090+Eden-Daniel@users.noreply.github.com>
2022-03-08 11:57:17 -0300

    adicionado funções

--- primeiroApp/src/app/tab1/tab1.page.ts
+++ primeiroApp/src/app/tab1/tab1.page.ts
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+  //variaves
+  paginas;
+  conometro;
+  tempo;
+  controleButao = false; 
+
+  start(){
+
+    if(this.paginas > 0 && this.paginas != null){
+      this.controleButao = true;
+      this.tempo = 0;
+      this.conometro = setInterval(() => {
+        this.tempo++
+        console.log(this.paginas)
+      }, 1000)
+    } else{
+      alert('informe uma quantidade de paginas validas!')
+    }
+
+  }
+
+  stop(){
+    this.controleButao = false;
+    clearInterval(this.conometro)
+  }
 
 }

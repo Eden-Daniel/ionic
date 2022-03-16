884d6c3d4e78e9d15dd85289ea28228e4ba58031
Éden Silva <91293090+Eden-Daniel@users.noreply.github.com>
2022-03-16 19:42:55 -0300

    finalizado primeira parte

--- primeiroApp/src/app/tab1/tab1.page.ts
+++ primeiroApp/src/app/tab1/tab1.page.ts
@@ -12,7 +12,8 @@ export class Tab1Page {
   paginas;
   conometro;
   tempo;
-  controleButao = false; 
+  controleButao = false;
+  resultado = null; 
 
   start(){
 
@@ -20,8 +21,7 @@ export class Tab1Page {
       this.controleButao = true;
       this.tempo = 0;
       this.conometro = setInterval(() => {
-        this.tempo++
-        console.log(this.paginas)
+        this.tempo++        
       }, 1000)
     } else{
       alert('informe uma quantidade de paginas validas!')
@@ -32,6 +32,23 @@ export class Tab1Page {
   stop(){
     this.controleButao = false;
     clearInterval(this.conometro)
+    this.mostrarResultado()
+    
+
   }
 
+  mostrarResultado(){
+    this.tempo = this.tempo * this.paginas;  //multiplicado segundos por paginas
+    // convertendo para tipo date, trasformando em mile segundo e usar funçao para mostrar em hh:mm:ss
+    this.resultado = new Date(this.tempo * 1000).toISOString().substr(11, 8)
+
+  }
+
+  reiniciar(){
+    this.paginas = null;
+    this.tempo = 0;
+    this.controleButao = false
+    this.resultado = null
+  }
+ 
 }
